46eac714bdb80d2a39719cd6f4887cd1eca42b85
irimiaandreea <andreea6gabriela@gmail.com>
2024-03-28 01:06:50 +0200

    add custom_exceptions.py and adapt the entrypoint.py file to handle the errors

--- model/qa_system/database.py
+++ model/qa_system/database.py
@@ -1,13 +1,19 @@
 import model.config.constants as constants
 import json
+import psycopg2
+from model.exceptions.custom_exceptions import DatabaseError
 
 
 def insert_into_embeddings(conn, cursor, current_question, current_answer, current_question_embedding,
                            current_answer_embedding):
-    cursor.execute(constants.INSERT_INTO_EMBEDDINGS_TABLE_QUERY,
-                   (current_question, json.dumps(current_question_embedding), current_answer,
-                    json.dumps(current_answer_embedding)))
-    conn.commit()
+    try:
+        cursor.execute(constants.INSERT_INTO_EMBEDDINGS_TABLE_QUERY,
+                       (current_question, json.dumps(current_question_embedding), current_answer,
+                        json.dumps(current_answer_embedding)))
+        conn.commit()
+    except psycopg2.Error as exception:
+        conn.rollback()
+        raise DatabaseError(f"Error inserting data into database: {exception}")
 
 
 def create_embeddings_table(conn, cursor):
--- model/qa_system/entrypoint.py
+++ model/qa_system/entrypoint.py
@@ -6,6 +6,8 @@ import model.config.constants as constants
 import database as db
 import numpy as np
 from scipy.spatial.distance import cosine
+from openai import OpenAIError
+from model.exceptions.custom_exceptions import *
 
 load_dotenv(dotenv_path="../config/.env")
 
@@ -22,10 +24,8 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 def check_if_openai_api_key_exists():
     if os.getenv("OPENAI_API_KEY"):
-        print("OPENAI_API_KEY is ready")  # todo delete all prints
         return True
     else:
-        print("OPENAI_API_KEY environment variable not found")
         return False
 
 
@@ -47,7 +47,7 @@ def compute_embeddings(conn, faq_database):
                                           current_answer_embedding)
 
             else:
-                raise ValueError(f"Error: {response.status_code}")
+                raise EmbeddingError(f"Error computing embeddings: {response.status_code}")
 
 
 def process_embeddings_for_user(user_query):
@@ -58,7 +58,7 @@ def process_embeddings_for_user(user_query):
         user_query_embeddings = data[0]["embedding"]
         return user_query_embeddings
     else:
-        raise ValueError(f"Error: {response.status_code}")
+        raise EmbeddingError(f"Error computing embeddings: {response.status_code}")
 
 
 # Function to compute embeddings for a given input data using OpenAI API
@@ -71,13 +71,19 @@ def get_embeddings(input_data):
         "Content-Type": constants.CONTENT_TYPE,
         "Authorization": f"Bearer {OPENAI_API_KEY}"
     }
-    response = requests.post(constants.OPENAI_API_URL_EMBEDDINGS, json=payload, headers=headers)
-    return response
+
+    try:
+        response = requests.post(constants.OPENAI_API_URL_EMBEDDINGS, json=payload, headers=headers)
+        return response
+    except requests.exceptions.RequestException as exception:
+        raise RequestError(f"Error occurred during HTTP request: {exception}")
+    except OpenAIError as exception:
+        raise OpenAIError(f"Error occurred during OpenAI API request: {exception}")
 
 
 # STEP 2 - Similarity Search
 # finds the most similar question and  compute the similarity between a user's query and the questions in the FAQ database
-def similarity_search(user_query_embedding, faq_questions_embeddings, faq_questions, faq_answers,
+def similarity_search(user_query_embedding, faq_questions_embeddings, faq_answers,
                       similarity_threshold):
     similarities = [1 - cosine(user_query_embedding, faq_question_embedding) for faq_question_embedding in
                     faq_questions_embeddings]
@@ -88,9 +94,9 @@ def similarity_search(user_query_embedding, faq_questions_embeddings, faq_questi
     if valid_similarity_scores:
         max_similarity_index = np.argmax(similarities)
         max_similarity_score = similarities[max_similarity_index]
-        return faq_questions[max_similarity_index], faq_answers[max_similarity_index], max_similarity_score
+        return faq_answers[max_similarity_index], max_similarity_score
     else:
-        return None, None, None
+        return None, None
 
 
 def decide_use_openai(similarity_score, similarity_threshold):
@@ -115,28 +121,31 @@ def get_answer_from_openai(user_query):
         "Content-Type": constants.CONTENT_TYPE,
         "Authorization": f"Bearer {OPENAI_API_KEY}"
     }
-    response = requests.post(constants.OPENAI_API_URL_COMPLETIONS, json=payload, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        # Extract the response from the API
-        if data.get("choices"):
-            return data["choices"][0]["message"]["content"]
-        else:
-            return None  # Handle case when API response does not contain choices
-    else:
-        # Handle API error
-        return None
+
+    try:
+        response = requests.post(constants.OPENAI_API_URL_COMPLETIONS, json=payload, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            # Extract the response from the API
+            if data.get("choices"):
+                return data["choices"][0]["message"]["content"]
+            else:
+                return "There is no response from OpenAI API."  # Handle case when API response does not contain choices
+    except requests.exceptions.RequestException as exception:
+        raise RequestError(f"Error occurred during HTTP request: {exception}")
+    except OpenAIError as exception:
+        raise OpenAIError(f"Error occurred during OpenAI API request: {exception}")
 
 
-def process_query(conn, user_query, similarity_threshold):
+def process_user_query(conn, user_query, similarity_threshold):
     user_query_embedding = process_embeddings_for_user(user_query)
 
     faq_questions, faq_questions_embeddings, faq_answers = db.retrieve_embeddings_from_database(conn)
 
-    best_match_question, best_match_answer, similarity_score = similarity_search(user_query_embedding,
-                                                                                 faq_questions_embeddings,
-                                                                                 faq_questions, faq_answers,
-                                                                                 similarity_threshold)
+    answer_from_local_faq, similarity_score = similarity_search(user_query_embedding,
+                                                                faq_questions_embeddings,
+                                                                faq_answers,
+                                                                similarity_threshold)
 
     use_openai = decide_use_openai(similarity_score, similarity_threshold)
 
@@ -144,12 +153,16 @@ def process_query(conn, user_query, similarity_threshold):
         answer_from_openai = get_answer_from_openai(user_query)
         return answer_from_openai
     else:
-        return best_match_answer
+        return answer_from_local_faq
 
 
 if __name__ == "__main__":
-    if (check_if_openai_api_key_exists()):  # TRUE
-        with psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT) as conn:
+    try:
+        if not check_if_openai_api_key_exists():  # TRUE
+            raise NoOpenAIKeyError("No OpenAI API key found")
+
+        with psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST,
+                              port=DB_PORT) as conn:
             with conn.cursor() as cursor:
                 if not db.table_exists(conn, cursor):
                     db.create_embeddings_table(conn, cursor)
@@ -159,9 +172,18 @@ if __name__ == "__main__":
         compute_embeddings(conn, constants.FAQ_DATABASE)
 
         user_query = "What fruits have blue color?"
-        answer = process_query(conn, user_query, constants.SIMILARITY_THRESHOLD)
+        answer = process_user_query(conn, user_query, constants.SIMILARITY_THRESHOLD)
         print("Question: ", user_query)
         print("Answer:", answer)
 
         cursor.close()
         conn.close()
+
+    except NoOpenAIKeyError as exception:
+        print(f"No OpenAI API key found: {exception}")
+    except psycopg2.OperationalError as exception:
+        print(f"Database error: {exception}")
+    except OpenAIError as exception:
+        print(f"OpenAI error: {exception}")
+    except Exception as exception:
+        print(f"An unexpected error occurred: {exception}")
