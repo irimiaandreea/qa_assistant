0eb8000ba22119e6fec03fee02ec119ae2ee2cc2
irimiaandreea <andreea6gabriela@gmail.com>
2024-03-29 02:43:49 +0200

    refactor and clean the code

--- components/qa_system/faq_search.py
+++ components/qa_system/faq_search.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 from openai import OpenAIError
 from scipy.spatial.distance import cosine
 
-import model.config.constants as constants
-import model.qa_system.database as db
-from model.exceptions.custom_exceptions import *
+import components.config.constants as constants
+import components.qa_system.database_operations as db
+from components.exceptions.custom_exceptions import *
 
-load_dotenv(dotenv_path="model/config/.env")
+load_dotenv(dotenv_path="../config/.env")
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 
@@ -35,13 +35,13 @@ def compute_embeddings(conn, faq_database):
                 raise EmbeddingError(f"Error computing embeddings: {response.status_code}")
 
 
-def process_embeddings_for_user(user_query):
-    response = get_embeddings(user_query)
+def process_embeddings_for_user(user_question):
+    response = get_embeddings(user_question)
 
     if response.status_code == 200:
         data = response.json()["data"]
-        user_query_embeddings = data[0]["embedding"]
-        return user_query_embeddings
+        user_question_embeddings = data[0]["embedding"]
+        return user_question_embeddings
     else:
         raise EmbeddingError(f"Error computing embeddings: {response.status_code}")
 
@@ -112,23 +112,27 @@ def get_answer_from_openai(user_query):
         response = requests.post(constants.OPENAI_API_URL_COMPLETIONS, json=payload, headers=headers)
         if response.status_code == 200:
             data = response.json()
-            # Extract the response from the API
+
             if data.get("choices"):
                 return data["choices"][0]["message"]["content"]
             else:
-                return "There is no response from OpenAI API."  # Handle case when API response does not contain choices
+                return "There is no response from OpenAI API."
     except requests.exceptions.RequestException as exception:
         raise RequestError(f"Error occurred during HTTP request: {exception}")
     except OpenAIError as exception:
         raise OpenAIError(f"Error occurred during OpenAI API request: {exception}")
 
 
-def process_user_query(conn, user_query, similarity_threshold):
-    user_query_embedding = process_embeddings_for_user(user_query)
+def process_user_query(conn, user_question, similarity_threshold):
+    user_question_embedding = process_embeddings_for_user(user_question)
 
-    faq_questions, faq_questions_embeddings, faq_answers = db.retrieve_embeddings_from_database(conn)
+    embeddings = db.retrieve_embeddings_from_database(conn)
 
-    answer_from_local_faq, question_from_local_faq, similarity_score = similarity_search(user_query_embedding,
+    faq_questions = [embedding.question for embedding in embeddings]
+    faq_questions_embeddings = [embedding.question_embedding for embedding in embeddings]
+    faq_answers = [embedding.answer for embedding in embeddings]
+
+    answer_from_local_faq, question_from_local_faq, similarity_score = similarity_search(user_question_embedding,
                                                                                          faq_questions,
                                                                                          faq_questions_embeddings,
                                                                                          faq_answers,
@@ -137,7 +141,7 @@ def process_user_query(conn, user_query, similarity_threshold):
     use_openai = decide_use_openai(similarity_score, similarity_threshold)
 
     if use_openai:
-        answer_from_openai = get_answer_from_openai(user_query)
+        answer_from_openai = get_answer_from_openai(user_question)
         return "openai", "N/A", answer_from_openai
     else:
         return "local", question_from_local_faq, answer_from_local_faq
